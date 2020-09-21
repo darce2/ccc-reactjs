f407d37cb524768d02ad878556f1f495b50b2dd4
Arlen Darcy <arlen@Arlens-MacBook-Pro.local>
2020-09-21 11:06:05 -0500

    starter

---
+++ src/Components/App/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+
+import App from './App';
+
+describe('App', () => {
+  it('renders', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('.App').exists()).toBe(true);
+  });
+});
